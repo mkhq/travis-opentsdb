de2b90cfc74f963a12b3073fe60252da7d2e70f8
Mikael Högqvist <mikael@peerialism.com>
2013-12-15 21:40:21 +0100

    Fixes to opentsdb install with patch for asynchbase 1.5.0-rc2 and run bootstrap before build

--- install-opentsdb.sh
+++ install-opentsdb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-wget https://github.com/OpenTSDB/opentsdb/releases/download/v2.0.0RC2/opentsdb-2.0.0.tar.gz
+#wget https://github.com/OpenTSDB/opentsdb/releases/download/v2.0.0RC2/opentsdb-2.0.0.tar.gz
 tar xzf opentsdb-2.0.0.tar.gz
 cd opentsdb-2.0.0
 patch -p1 < ../opentsdb-protobuf.patch
-patch -p0 < ../opentsdb-conf.patch
-./build.sh
-cp ../asynchbase-1.4.1.jar third_party/hbase/
+patch -p0 < ../opentsdb.patch
+rm third_party/hbase/asynchbase-1.4.1.jar*
+cp ../asynchbase-1.5.0-rc2.jar* third_party/hbase/
+./bootstrap
 ./build.sh
 cd ..
