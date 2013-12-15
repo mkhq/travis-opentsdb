209f9a05d4b28aae76e2b32f0ba213fdbdb3e1df
Mikael Högqvist <mikael@peerialism.com>
2013-12-15 16:05:52 +0100

    Copy jars after build completes

--- install-opentsdb.sh
+++ install-opentsdb.sh
@@ -5,6 +5,6 @@ tar xzf opentsdb-2.0.0.tar.gz
 cd opentsdb-2.0.0
 patch -p1 < ../opentsdb-protobuf.patch
 patch -p0 < ../opentsdb-conf.patch
-cp ../asynchbase-1.4.1.jar build/third_party/hbase/
 ./build.sh
+cp ../asynchbase-1.4.1.jar build/third_party/hbase/
 cd ..
